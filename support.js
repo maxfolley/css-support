0e0768b54be65fc65bcb48fa0b3e19524ff66033
maxfolley <maxwell.folley@genuinemade.co>
2012-06-12 08:52:33 -0700

    init

---
+++ support.js
@@ -0,0 +1,93 @@
+var SUPPORT = (function () {
+    var animationID = Modernizr.prefixed("animation"), 
+        transformID = Modernizr.prefixed("transform");
+    
+    var that = {
+        animation: {
+            name: '',
+            endEvent: '',
+            playState: ''
+        },
+        transition: {
+            delay: '',
+            duration: '',
+            endEvent: '',
+            property: '',
+            shortHand: '',
+            timingFunction: ''
+        },
+        transform: Modernizr.prefixed("transform")
+    }
+    switch(Modernizr.prefixed("animation")) {
+        case 'WebkitAnimation':
+            that.animation.name = '-webkit-animation-name'; 
+            that.animation.endEvent = 'webkitAnimationEnd'; 
+            that.animation.playState = '-webkit-animation-play-state'; 
+            break;
+
+        case 'MozAnimation':
+            that.animation.name = '-moz-animation-name'; 
+            that.animation.endEvent = 'animationend';
+            that.animation.playState = '-moz-animation-play-state'; 
+            break;
+        
+        case 'animation':
+            that.animation.name = 'animation-name'; 
+            that.animation.endEvent = 'animationend';
+            that.animation.playState = 'animation-play-state'; 
+            break;
+
+        default:
+            break;
+    }
+    switch(Modernizr.prefixed("transition")) {
+       case 'WebkitTransition':
+            that.transition.property = '-webkit-transition-property'; 
+            that.transition.duration = '-webkit-transition-duration'; 
+            that.transition.timingFunction = '-webkit-transition-timing-function';
+            that.transition.delay = '-webkit-transition-delay';
+            that.transition.shortHand = '-webkit-transition';
+            that.transition.endEvent = 'webkitTransitionEnd'; 
+            break;
+
+        case 'MozTransition':
+            that.transition.property = '-moz-transition-property'; 
+            that.transition.duration = '-moz-transition-duration'; 
+            that.transition.timingFunction = '-moz-transition-timing-function';
+            that.transition.delay = '-moz-transition-delay';
+            that.transition.shortHand = '-moz-transition';
+            that.transition.endEvent = 'transitionend';
+            break;
+        
+        case 'OTransition':
+            that.transition.property = '-o-transition-property'; 
+            that.transition.duration = '-o-transition-duration'; 
+            that.transition.timingFunction = '-o-transition-timing-function';
+            that.transition.delay = '-o-transition-delay';
+            that.transition.shortHand = '-o-transition';
+            that.transition.endEvent = 'oTransitionEnd';
+            break;
+
+        case 'msTransition':
+            that.transition.property = '-ms-transition-property'; 
+            that.transition.duration = '-ms-transition-duration'; 
+            that.transition.timingFunction = '-ms-transition-timing-function';
+            that.transition.delay = '-ms-transition-delay';
+            that.transition.shortHand = '-ms-transition';
+            that.transition.endEvent = 'MSTransitionEnd';
+            break;
+
+        case 'transition':
+            that.transition.property = 'transition-property'; 
+            that.transition.duration = 'transition-duration'; 
+            that.transition.timingFunction = 'transition-timing-function';
+            that.transition.delay = 'transition-delay';
+            that.transition.shortHand = 'transition';
+            that.animation.endEvent = 'transitionend'; 
+            break;
+
+        default:
+            break;
+    }
+    return that;
+}());
